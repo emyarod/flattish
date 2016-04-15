12f4639843d941179efece0e1372cf0ee5990d80
emyarod <allllaboutyou@gmail.com>
2016-04-14 18:47:17 -0700

    test .nojekyll

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.ajax({
     // url: 'style/main.scss'
-    url: 'style/utils/_import.scss'
+    url: 'style/_utils/_import.scss'
   })
   .done(function(data) {
     $('#target').html(data)
