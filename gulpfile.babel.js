814a4278e704bf75513fc70e5a73f848acda94fe
emyarod <allllaboutyou@gmail.com>
2016-04-26 20:39:43 -0700

    add comments to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,16 +10,22 @@ const paths = {
   dest: 'app',
 };
 
+// scripts
 gulp.task('scripts', () => {
   return gulp.src(`${paths.src}/js/**/*.js`)
   .pipe(babel())
   .pipe(gulp.dest(`${paths.dest}/js/`));
 });
 
+// clean
 gulp.task('clean', () => {
   return del([`${paths.dest}/js`]);
 });
 
+
+// default
 gulp.task('default', ['clean'], () => {
   gulp.start('scripts');
 });
+
+// watch
