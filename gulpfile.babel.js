69b75e4f81295892f73dd4ab05c9c0623ff44da1
emyarod <allllaboutyou@gmail.com>
2016-05-08 18:55:33 -0700

    Implement browsersync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,8 @@
 // include gulp and tools used
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import uglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
 import del from 'del';
 
 const paths = {
@@ -10,22 +12,44 @@ const paths = {
   dest: 'app',
 };
 
-// scripts
-gulp.task('scripts', () => {
-  return gulp.src(`${paths.src}/js/**/*.js`)
-  .pipe(babel())
-  .pipe(gulp.dest(`${paths.dest}/js/`));
-});
-
-// clean
+// clean out destination folders and rebuild from source
 gulp.task('clean', () => {
   return del([`${paths.dest}/js`]);
 });
 
-
-// default
-gulp.task('default', ['clean'], () => {
-  gulp.start('scripts');
+/**
+ * transpile from ES2015 via babel
+ * uglify
+ * output to destination
+ */
+gulp.task('js', ['clean'], () => {
+  return gulp.src(`${paths.src}/js/**/*.js`)
+  .pipe(babel())
+  // .pipe(uglify())
+  .pipe(gulp.dest(`${paths.dest}/js/`));
 });
 
-// watch
+/**
+ * watchers
+ *
+ * create a task that ensures the `js` task completes before reloading browsers
+ */
+gulp.task('js-watch', ['js'], browserSync.reload);
+
+// default
+gulp.task('default', ['js'], () => {
+  // static server
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+    browser: 'chrome',
+    port: 8080,
+  });
+
+  /**
+   * add browserSync.reload to the tasks array to make
+   * all browsers reload after tasks are complete
+   */
+  gulp.watch(`${paths.src}/js/**/*.js`, ['js-watch']);
+});
