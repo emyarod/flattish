107d64a4809a030dd04c9a096f2f1d55a1ee742f
emyarod <allllaboutyou@gmail.com>
2016-05-18 02:10:03 -0700

    Edit compile button tooltip properties

--- src/js/main/app.js
+++ src/js/main/app.js
@@ -1086,10 +1086,14 @@ $('#rotating-header-checkbox:checkbox').change(() => {
 function compileButtonEnabler(state) {
   if (state === 'enable') {
     $('#compile').removeClass('disabled').prop('disabled', false);
-    $('#compile-div').removeClass('disabled').tooltip('destroy');
+    $('#compile-div').removeClass('disabled')
+      .attr('title', '')
+      .tooltip('destroy');
   } else if (state === 'disable') {
     $('#compile').addClass('disabled').prop('disabled', true);
-    $('#compile-div').addClass('disabled').tooltip();
+    $('#compile-div').addClass('disabled')
+      .attr('title', 'Please correct all errors!')
+      .tooltip();
   }
 }
 
