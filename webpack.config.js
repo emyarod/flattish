c35bc6443bab04028e7c29530e513afbf8275f1d
emyarod <allllaboutyou@gmail.com>
2016-05-20 00:01:57 -0700

    Add jQuery plugin Update module loaders to support Bootstrap and Sass files

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,47 @@
 import webpack from 'webpack';
-// var del = require('clean-webpack-plugin');
 
 module.exports = {
   context: __dirname,
-  entry: './src/js/main/app.js',
+  entry: ['bootstrap-loader', './src/js/main/app.js'],
   output: {
     path: `${__dirname}/app/js`,
     filename: 'bundle.js'
   },
-  externals: {
-    jquery: 'jQuery',
-  },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-    }],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+      {
+        test: /\.woff$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
+      },
+      {
+        test: /\.woff2$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]",
+      },
+      {
+        test: /\.(eot|ttf|svg|gif|png)$/,
+        loader: "file-loader",
+      },
+    ],
   },
   plugins: [
-    // new del(['dist/js']),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin(),
