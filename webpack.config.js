c45dc0d0b72581fcc96cfaf7e9f2269a1e8c3245
emyarod <allllaboutyou@gmail.com>
2016-05-20 19:04:09 -0700

    Remove jQuery and Bootstrap from bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 
 module.exports = {
   context: __dirname,
-  entry: ['bootstrap-loader', './src/js/main/app.js'],
+  entry: './src/js/main/app.js',
   output: {
     path: `${__dirname}/app/js`,
     filename: 'bundle.js'
@@ -12,7 +12,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel',
       },
       {
         test: /\.css$/,
@@ -22,26 +22,12 @@ module.exports = {
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass'],
       },
-      {
-        test: /\.woff$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
-      },
-      {
-        test: /\.woff2$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]",
-      },
-      {
-        test: /\.(eot|ttf|svg|gif|png)$/,
-        loader: "file-loader",
-      },
     ],
   },
+  externals: {
+    jquery: 'jQuery',
+  },
   plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin(),
