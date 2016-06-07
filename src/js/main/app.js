7863a421e0cf889d39a3cb036828d5456ea373ad
emyarod <allllaboutyou@gmail.com>
2016-06-06 21:57:50 -0700

    Move addon labeler to module

--- src/js/main/app.js
+++ src/js/main/app.js
@@ -286,38 +286,7 @@ function replacer(inputString, varType, replacementValue, variable = null) {
   }
 }
 
-// addon labels
-var addonLabeler = {
-  addons: [
-    'large-header',
-    'rotating-header',
-    'sidebar-img',
-    'pinned-topics',
-  ],
-  enable: (addon) => {
-    $(`#${addon}-label`)
-      .addClass('label-info')
-      .removeClass('label-default')
-      .html('Enabled');
-
-  },
-  disable: (addon) => {
-    $(`#${addon}-label`)
-      .addClass('label-default')
-      .removeClass('label-info')
-      .html('Disabled');
-  }
-}
-
-addonLabeler.addons.forEach((element, index, array) => {
-  $(`#${element}-checkbox:checkbox`).change(() => {
-    if ($(`#${element}-checkbox:checkbox`).prop('checked')) {
-      addonLabeler.enable(element);
-    } else {
-      addonLabeler.disable(element);
-    }
-  });
-});
+import './addons/addons.js';
 
 // large header
 $('#large-header-checkbox:checkbox').change(() => {
