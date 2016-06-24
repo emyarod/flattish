e00af56682e681a5d91b5570a8b376b42bb6a4ca
emyarod <allllaboutyou@gmail.com>
2016-06-24 04:09:13 -0700

    Update build process to compile and minify Sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,41 @@
 'use strict';
 
 import gulp from 'gulp';
+import sass from 'gulp-sass';
+import rename from 'gulp-rename';
 import del from 'del';
 
 // clean out destination folders
-gulp.task('clean', () => {
+gulp.task('clean', ['minify', 'sass'], () => {
   return del(['./dist']);
 });
 
-// pipe to `dist`
+// compile to css
+gulp.task('sass', () => {
+  gulp.src('./style/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(gulp.dest((f) => {
+      return f.base;
+    }));
+});
+
+// generate minified css
+gulp.task('minify', () => {
+  gulp.src('./style/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(rename((path) => {
+      path.basename += '.min';
+    }))
+    .pipe(gulp.dest((f) => {
+      return f.base;
+    }));
+});
+
+// pipe css to `dist`
 gulp.task('css', ['clean'], () => {
   return gulp.src('./style/flattish.min.css')
     .pipe(gulp.dest('./dist/'));
@@ -29,7 +56,7 @@ gulp.task('images', ['clean'], () => {
   });
 });
 
-// pipe to `dist`
+// pipe README to `dist`
 gulp.task('readme', ['clean'], () => {
   return gulp.src('./README.md')
     .pipe(gulp.dest('./dist/'));
@@ -37,5 +64,6 @@ gulp.task('readme', ['clean'], () => {
 
 // run `create-dist` on stylesheet or readme change
 gulp.task('default', ['css', 'images', 'readme'], () => {
+  gulp.watch('./**/*.scss', ['css']);
   gulp.watch(['./style/flattish.min.css', './README.md'], ['css', 'images', 'readme']);
 });
